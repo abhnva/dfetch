54c6a40e475e86429e5be2f040ffdfd4523e6fed
Astro Rabbit <93075695+abhnva@users.noreply.github.com>
2022-05-09 18:24:11 +0530

    Added Username and DE

--- dfetch.sh
+++ dfetch.sh
@@ -13,6 +13,8 @@ echo -e "\t ${CYAN}Codename${END} : ${RED}$(lsb_release -cs)${END}"
 echo -e "\t ${CYAN}Architecture${END} : ${RED}$(uname -m)${END}"
 echo -e "\t ${CYAN}Uptime${END} : ${RED}$(uptime -p)${END}"
 echo -e "\t ${CYAN}Hostname${END} : ${RED}$(hostname)${END}"
+echo -e "\t ${CYAN}User${END} : ${RED}$(whoami)${END}"
+echo -e "\t ${CYAN}Desktop${END} : ${RED}$(echo $DESKTOP_SESSION)${END}"
 echo -e "\t ${CYAN}Kernel${END} : ${RED}$(uname -r)${END}"
 echo -e "\t ${CYAN}CPU${END} : ${RED}$(lscpu | grep "Model name" | awk '{print $3,$4,$5}') | Core(s): $(nproc)${END}"
 echo -e "\t ${CYAN}RAM${END} : ${RED}$(free -h | grep "Mem" | awk '{print $2}')"${END} 
